9e7dbd55fdfbf2008ed727699bf7fff0d4e52254
daniellrgn <daniellrgn@gmail.com>
2016-12-01 10:35:37 -0800

    Add files via upload

--- Structures.java
+++ Structures.java
@@ -1,11 +1,20 @@
 import java.io.*;
 import java.util.*;
+import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class Structures{
   private LinkedList<Name> maleList;
   private LinkedList<Name> femaleList;
-  private int totalMales = 0;
-  private int totalFemales = 0;
+  private int totalMales;
+  private int totalFemales;
+
+  public Structures(){
+    maleList = new LinkedList<Name>();
+    femaleList = new LinkedList<Name>();
+    totalMales = 0;
+    totalFemales = 0;
+  }
 
   public class Name {
     private String name;
@@ -40,21 +49,27 @@ public class Structures{
     return femaleList;
   }
 
-  public void loadLists(String nameFile){
+  public ArrayList<String> loadLists(String nameFile){
+    ArrayList<String> allNames = new ArrayList<String>();
     Scanner line = null;
     String name = "";
     String gender = "";
     int frequency = 0;
 
+
     try{
       File file = new File(nameFile);
       line = new Scanner(file);
-      while (line.hasNext()){
-        name = line.next();
-        gender = line.next();
-        frequency = Integer.parseInt(line.next());
+      while (line.hasNextLine()){
+        String l = line.nextLine();
+        Scanner lineData = new Scanner(l);
+        lineData.useDelimiter(",");
+        name = lineData.next();
+        gender = lineData.next();
+        frequency = lineData.nextInt();
+        allNames.add(name);
         Name n = new Name(name, gender, frequency);
-        if (gender == "M"){
+        if (gender.equals("M")){
           maleList.add(n);
           totalMales = totalMales + frequency;
         }else{
@@ -66,54 +81,52 @@ public class Structures{
       System.out.println("Error, unable to read file" + nameFile);
       System.exit(1);
     }
-    return;
+    return allNames;
   }
 
   public void searchNameList(String name){
     int femaleRank = 0;
     int fRank = 0;
     int femaleFreq = 0;
+    int maleRank = 0;
+    int mRank = 0;
+    int maleFreq = 0;
+
     for (Name n : femaleList){
       fRank++;
-      if (n.getName() == name){
+      if (n.getName().equals(name)){
         femaleFreq = n.getFreq();
         femaleRank = fRank;
         break;
       }
     }
 
-    int maleRank = 0;
-    int mRank = 0;
-    int maleFreq = 0;
     for (Name n : maleList){
       mRank++;
-      if(n.getName() == name){
+      if (n.getName().equals(name)){
         maleFreq = n.getFreq();
         maleRank = mRank;
         break;
       }
     }
-    System.out.printf("%-15s%-15s%-15s%-15s%-15s", "Year", "Female", "Female-Rank", "Male", "Male-Rank");
-    System.out.printf("%-15d%-15d%-15d%-15d%-15d", 2014, femaleFreq, femaleRank, maleFreq, maleRank);
-    /*System.out.println("Year: 2014");
-    System.out.println("Female Frequency: " + femaleFreq);
-    System.out.println("Female Rank:      " + femaleRank);
-    System.out.println("Male Frequency:   " + maleFreq);
-    System.out.println("Male Rank:        " + maleRank + "\n");*/
-  }
+    System.out.println("Selected Name: " + name);
+    System.out.printf("%-15s%-15s%-15s%-15s%-15s%n", "Year", "# Females", "Female-Rank", "# Males", "Male-Rank");
+    System.out.printf("%-15d%-15d%-15d%-15d%-15d%n", 2014, femaleFreq, femaleRank, maleFreq, maleRank);
+    }
 
   public void mostPopularNameList(){
     Name f = null;
     Name m = null;
     int fPercent = 0;
     int mPercent = 0;
-    System.out.printf("%-15s%-15s%-15s%-15s%-15s%-15s", "Female Name", "Frequency", "%", "Male Name", "Frequency", "%");
+    System.out.printf("%-15s%-15s%-15s%-15s%-15s%-15s%n", "Female Name", "Frequency", "%", "Male Name", "Frequency", "%");
     for (int i=0; i<10; i++){
       f = femaleList.get(i);
-      fPercent = ((totalFemales*100)/f.getFreq());
+      fPercent = ((f.getFreq()*100)/totalFemales);
       m = maleList.get(i);
-      mPercent = ((totalMales*100)/m.getFreq());
-      System.out.printf("%-15s%-15d%-15d%%%-15s%-15d%-15d%%", f.getName(), f.getFreq(), fPercent, m.getName(), m.getFreq(), mPercent);
+      mPercent = ((m.getFreq()*100)/totalMales);
+      System.out.printf("%-15s%-15d%-15s%-15s%-15d%-15s%n", f.getName(), f.getFreq(), fPercent+"%", m.getName(), m.getFreq(), mPercent+"%");
+
     }
 
   }
@@ -123,13 +136,13 @@ public class Structures{
     Name m = null;
     int fPercent = 0;
     int mPercent = 0;
-    System.out.printf("%-15s%-15s%-15s%-15s%-15s%-15s", "Female Name", "Frequency", "%", "Male Name", "Frequency", "%");
-    for (int i=0; i<5; i++){
+    System.out.printf("%-15s%-15s%-15s%-15s%-15s%-15s%n", "Female Name", "Frequency", "%", "Male Name", "Frequency", "%");
+    for (int i=1; i<6; i++){
       f = femaleList.get(femaleList.size()-i);
-      fPercent = ((totalFemales*100)/f.getFreq());
+      fPercent = ((f.getFreq()*100)/totalFemales);
       m = maleList.get(maleList.size()-i);
-      mPercent = ((totalMales*100)/m.getFreq());
-      System.out.printf("%-15s%-15d%-15d%%%-15s%-15d%-15d%%", f.getName(), f.getFreq(), fPercent, m.getName(), m.getFreq(), mPercent);
+      mPercent = ((m.getFreq()*100)/totalMales);
+      System.out.printf("%-15s%-15d%-15s%-15s%-15d%-15s%n", f.getName(), f.getFreq(), fPercent+"%", m.getName(), m.getFreq(), mPercent+"%");
     }
 
   }
@@ -143,7 +156,7 @@ public class Structures{
       fullList.add(n);
     }
     Name[] A  = fullList.toArray(new Name[fullList.size()]);
-    Sort(A, 0, fullList.size());  //alphabetize the names
+    Sort(A, 0, fullList.size()-1);  //alphabetize the names
     LinkedList<Name> displayList = new LinkedList<Name>(Arrays.asList(A));
 
     int f = 0;
@@ -153,12 +166,12 @@ public class Structures{
     Name blank = new Name();
     Name curr = null;
 
-    System.out.printf("%-15s%-15s%-15s%-15s%-15s", "Name", "F Frequency", "% Females", "M Frequency", "% Males");
+    System.out.printf("%-15s%-15s%-15s%-15s%-15s%n", "Name", "F Frequency", "% Females", "M Frequency", "% Males");
 
     for (Name next : displayList){     //logic to format names. Looks at next and current
       if (curr != null){
-        if (curr.getName() == next.getName()){
-          if (next.gender == "M"){
+        if (curr.getName().equals(next.getName())){
+          if (next.gender.equals("M")){
             m = next.getFreq();
             f = curr.getFreq();
           }else{
@@ -166,7 +179,7 @@ public class Structures{
             m = curr.getFreq();
           }
         }else{
-          if (curr.gender == "M"){
+          if (curr.gender.equals("M")){
             m = curr.getFreq();
             f= blank.getFreq();
           }else{
@@ -174,9 +187,9 @@ public class Structures{
             m = blank.getFreq();
           }
         }
-        mPercent = ((totalMales*100)/m);
-        fPercent = ((totalFemales*100)/f);
-        System.out.printf("%-15s%-15d%-15d%%%-15d%-15d%%", curr.getName(), f, fPercent, m, mPercent);
+        mPercent = ((m*100)/totalMales);
+        fPercent = ((f*100)/totalFemales);
+        System.out.printf("%-15s%-15d%-15s%-15d%-15s%n", curr.getName(), f, fPercent+"%", m, mPercent+"%");
       }
       curr = next;
     }
